51dcc9b31b837ad4f1c4a4c1dba24014bf9908e2
simenhaga <simen.haga@lyse.net>
2022-12-05 13:09:42 +0100

    Trying to get dashboards in AWS CW

--- infra/dashboards.tf
+++ infra/dashboards.tf
@@ -1,7 +1,7 @@
 resource "aws_cloudwatch_dashboard" "main" {
   dashboard_name = var.candidate_id
 ## Jim; seriously! we can use any word here.. How cool is that?
-  dashboard_body = <<DEATHSTAR
+  dashboard_body = <<DASHBOARD
 {
   "widgets": [
     {
@@ -14,16 +14,35 @@ resource "aws_cloudwatch_dashboard" "main" {
         "metrics": [
           [
             "${var.candidate_id}",
-            "account_count.value"
+            "cartCount.value"
           ]
         ],
         "period": 300,
         "stat": "Maximum",
         "region": "eu-west-1",
-        "title": "Total number of accounts"
+        "title": "Total number of carts"
+      }
+    },
+    {
+      "type": "metric",
+      "x": 0,
+      "y": 0,
+      "width": 12,
+      "height": 6,
+      "properties": {
+        "metrics": [
+          [
+            "${var.candidate_id}",
+            "cartSum.value"
+          ]
+        ],
+        "period": 300,
+        "stat": "Maximum",
+        "region": "eu-west-1",
+        "title": "Total sum in carts"
       }
     }
   ]
 }
-DEATHSTAR
+DASHBOARD
 }
--- src/main/java/no/shoppifly/ShoppingCartController.java
+++ src/main/java/no/shoppifly/ShoppingCartController.java
@@ -1,19 +1,31 @@
 package no.shoppifly;
 
+import io.micrometer.core.instrument.Gauge;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController()
-public class ShoppingCartController {
+public class ShoppingCartController implements ApplicationListener<ApplicationReadyEvent> {
+
+    private final CartService cartService;
+    private MeterRegistry meterRegistry;
+    private Map<String, NaiveCartImpl> theShop = new HashMap();
+
 
     @Autowired
-    private final CartService cartService;
-
-    public ShoppingCartController(CartService cartService) {
+    public ShoppingCartController(CartService cartService, MeterRegistry meterRegistry) {
         this.cartService = cartService;
+        this.meterRegistry = meterRegistry;
     }
+
 
     @GetMapping(path = "/cart/{id}")
     public Cart getCart(@PathVariable String id) {
@@ -27,6 +39,9 @@ public class ShoppingCartController {
      */
     @PostMapping(path = "/cart/checkout")
     public String checkout(@RequestBody Cart cart) {
+        // remove the gauge meterRegistry from aws cloudwatch
+
+
         return cartService.checkout(cart);
     }
 
@@ -38,6 +53,8 @@ public class ShoppingCartController {
      */
     @PostMapping(path = "/cart")
     public Cart updateCart(@RequestBody Cart cart) {
+        meterRegistry.counter("cartCount").increment();
+        meterRegistry.counter("cartSum").increment();
         return cartService.update(cart);
     }
 
@@ -48,8 +65,23 @@ public class ShoppingCartController {
      */
     @GetMapping(path = "/carts")
     public List<String> getAllCarts() {
+        meterRegistry.counter("cartCount").increment();
         return cartService.getAllsCarts();
     }
 
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent applicationReadyEvent) {
+
+        Gauge.builder("cartCount", theShop,
+                b -> b.values().size()).register(meterRegistry);
+
+        Gauge.builder("cartSum", theShop,
+                        b -> b.values()
+                                .stream()
+                                .map(NaiveCartImpl::total)
+                                .mapToDouble(Float::doubleValue)
+                                .sum())
+                .register(meterRegistry);
+    }
 
 }
