d3268a7c3699d004ea473d294d243c38368271b0
simenhaga <simen.haga@lyse.net>
2022-12-06 15:43:17 +0100

    Added 2 more dashboards for checkout and checkout_latency

--- infra/dashboards.tf
+++ infra/dashboards.tf
@@ -41,7 +41,45 @@ resource "aws_cloudwatch_dashboard" "main" {
         "region": "eu-west-1",
         "title": "Total sum in carts"
       }
-    }
+    },
+    {
+      "type": "metric",
+      "x": 0,
+      "y": 0,
+      "width": 12,
+      "height": 6,
+      "properties": {
+        "metrics": [
+          [
+            "${var.candidate_id}",
+            "checkout.value"
+          ]
+        ],
+        "period": 300,
+        "stat": "Maximum",
+        "region": "eu-west-1",
+        "title": "Total number of checkouts"
+      }
+    },
+    {
+      "type": "metric",
+      "x": 0,
+      "y": 0,
+      "width": 12,
+      "height": 6,
+      "properties": {
+        "metrics": [
+          [
+            "${var.candidate_id}",
+            "checkout_latency.value"
+          ]
+        ],
+        "period": 300,
+        "stat": "Maximum",
+        "region": "eu-west-1",
+        "title": "Checkout latency"
+      }
+    },
   ]
 }
 DASHBOARD
